1632a15236e66979b28662cfde998d28b78ce4c4
jeffstinson <jeffreymstinson@gmail.com>
2026-09-14 15:06:55 -0400

    feat: include datalog parser profiles in preflight

--- app/server/preflight.js
+++ app/server/preflight.js
@@ -8,7 +8,7 @@ const expectedTables = [
   "customers","vehicles","orders","tune_projects","project_requirements","revisions","logs","files","conversations","messages","events",
   "automation_rules","automation_runs","integration_sync_state","webhook_receipts","internal_users","customer_portal_users","external_links","outbound_actions",
   "import_batches","import_items","intake_requests","intake_access_tokens","integration_cutovers","schema_migrations","connection_tests","setup_checkpoints",
-  "vehicle_catalog","logging_recipes","parameter_pack_profiles","platform_workflow_profiles",
+  "vehicle_catalog","logging_recipes","parameter_pack_profiles","platform_workflow_profiles","log_parser_profiles",
 ];
 
 function ms(start){return Math.max(0,Date.now()-start)}
