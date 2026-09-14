091db8f8ed313d485fd3863cd69e2ac5ec8caf53
jeffstinson <jeffreymstinson@gmail.com>
2026-09-14 08:11:50 -0400

    chore: move Subpar logo into public assets

--- app/subpar-logo.png/route.js
+++
@@ -1,5 +0,0 @@
-import { NextResponse } from "next/server";
-
-export function GET() {
-  return NextResponse.redirect("https://raw.githubusercontent.com/jeffstinson/subpar/main/subpar-logo.png");
-}
